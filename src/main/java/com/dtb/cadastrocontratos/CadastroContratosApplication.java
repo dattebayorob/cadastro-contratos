68a3d842b7ad06b3282a5340324ed4200ff6b102
dattebayoRob <robson.william65@gmail.com>
2018-12-08 09:28:06 -0300

    Commit inicial

---
+++ src/main/java/com/dtb/cadastrocontratos/CadastroContratosApplication.java
@@ -0,0 +1,12 @@
+package com.dtb.cadastrocontratos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroContratosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroContratosApplication.class, args);
+	}
+}

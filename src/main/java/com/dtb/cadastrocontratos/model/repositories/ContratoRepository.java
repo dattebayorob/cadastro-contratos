2ab3323299b4e00e6a90167001884fedaa76d89d
dattebayoRob <robson.william65@gmail.com>
2018-12-12 14:55:40 -0300

    Deixando de reiventar a roda JpaRepository é de Deus...

--- src/main/java/com/dtb/cadastrocontratos/model/repositories/ContratoRepository.java
+++ src/main/java/com/dtb/cadastrocontratos/model/repositories/ContratoRepository.java
@@ -1,24 +1,11 @@
 package com.dtb.cadastrocontratos.model.repositories;
 
-import java.util.List;
-
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.dtb.cadastrocontratos.model.entities.Contrato;
 
-@NamedQueries({
-	@NamedQuery(name = "ContratoRepository.existsByContrato", 
-		query = "SELECT CASE WHEN EXISTS (SELECT c.contrato FROM contrato c WHERE contrato.contrato = :contrato) THEN 'true' ELSE 'false' END"),
-	@NamedQuery(name = "ContratoRepository.findByContrato", 
-		query = "SELECT c FROM contrato c WHERE c.contrato = :contrato")
-})
 public interface ContratoRepository extends JpaRepository<Contrato, Long> {
-	//List<Contrato> findByContrato(String contrato);
-	Contrato findByContrato(@Param("contrato") String contrato);
-	boolean existsByContrato(@Param("contrato") String contrato);
+	Contrato findByContrato(String contrato);
+	boolean existsByContrato(String contrato);
 
 }
--- src/test/java/com/dtb/cadastrocontratos/service/ContratoServiceTest.java
+++ src/test/java/com/dtb/cadastrocontratos/service/ContratoServiceTest.java
@@ -1,7 +1,8 @@
-package com.dtb.cadastrocontratos.service.impl;
+package com.dtb.cadastrocontratos.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
@@ -13,24 +14,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.dtb.cadastrocontratos.model.entities.Contrato;
-import com.dtb.cadastrocontratos.model.repositories.ContratoRepository;
-import com.dtb.cadastrocontratos.service.ContratoService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class ContratoServiceImplTest {
-	private static final String CONTRATO = "123456712123";
+public class ContratoServiceTest {
+	private static final String CONTRATO = "000000000004";
+	private static final Long ID = Long.valueOf(4);
 	@Autowired
 	private ContratoService service;
-	@Autowired
-	private ContratoRepository repository;
 	
 	
 	@Test
-	public void testContratoExistsContrato() {
-		Optional<Contrato> contratoPeloId = service.buscarPeloId(Long.valueOf(1));
+	public void testBuscarPeloIdEqualBuscarPeloContrato() {
+		Optional<Contrato> contratoPeloId = service.buscarPeloId(Long.valueOf(ID));
 		Contrato contrato = service.buscarPeloContrato(CONTRATO);
-		assertEquals(contratoPeloId.get().getDataVencimento(), contrato.getDataVencimento());
+		assertEquals(contratoPeloId.get().getId(), contrato.getId());
+	}
+	@Test
+	public void testBuscarPeloIdNotEqualBuscarPeloContrato() {
+		Optional<Contrato> contratoPeloId = service.buscarPeloId(Long.valueOf(ID+1));
+		Contrato contrato = service.buscarPeloContrato(CONTRATO);
+		assertNotEquals(contratoPeloId.get().getId(), contrato.getId());
 	}
 	@Test
 	public void testBooleanExistsContrato() {
